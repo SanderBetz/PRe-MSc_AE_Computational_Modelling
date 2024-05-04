2cfeba78e30d91ed6fc3b74cf12ea2d42b717ad6
SanderBetz <sander.betz@me.com>
2024-05-04 21:40:00 +0200

    Initial Commit to GitHub

---
+++ WelcomeFile.py
@@ -0,0 +1,13 @@
+# AUTHOR:   SANDER BETZ
+# ST-NR:    6070000
+# ST-MAIL:  S.H.R.Betz@student.tudelft.nl
+
+# Put imports here
+
+# Main code
+def main():
+    print('Hello, world!')
+
+
+if __name__ == '__main__':
+    main()
